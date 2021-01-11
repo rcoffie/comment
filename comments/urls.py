0f20b0d8f0a0cbbd7ca1d49bb86c28a7f19af8a6
Robert Coffie <rcoffie22@yahoo.com>
2021-01-11 14:22:29 +0000

    detilview

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .import views 
 urlpatterns = [
   path('',views.Home,name='home'),
+  path('<int:id>/',views.PostDetail,name='detail')
     
 ]
--- comments/views.py
+++ comments/views.py
@@ -8,3 +8,11 @@ def Home(request):
   posts = Post.objects.all()
   context = {'posts':posts}
   return render(request,'comments/index.html',context)
+
+
+
+def PostDetail(request, id):
+  post = Post.objects.get(id=id)
+  context = {'post':post}
+
+  return render(request,'comments/detail.html',context)
