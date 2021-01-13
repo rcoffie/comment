c801be09e98e5a75e1f94b5ea0fcc4452424f47e
Robert Coffie <rcoffie22@yahoo.com>
2021-01-13 14:56:08 +0000

    commenting from the frontend

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from .models import *
+from django.urls import reverse
 
 # Create your views here.
 
@@ -14,6 +15,13 @@ def Home(request):
 def PostDetail(request, id):
   post = Post.objects.get(id=id)
   comments = Comment.objects.filter(post=post).order_by('-id')
+
+  if request.method == 'POST':
+     content = request.POST.get('content')
+     comment = Comment.objects.create(user=request.user, post=post, content=content)
+     comment.save()
+     return HttpResponseRedirect(reverse('detail', args=[post.id]))
+
   context = {'post':post,'comments':comments}
 
   return render(request,'comments/detail.html',context)
